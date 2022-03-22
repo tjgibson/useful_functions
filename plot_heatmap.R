9bc77113636aa106059762abfd11ec860e5b4878
Tyler Gibson <tylergibson@pa2.medmicro.wisc.edu>
2022-03-22 08:10:26 -0500

    added plot_average function

--- plot_heatmap.R
+++ plot_heatmap.R
@@ -50,6 +50,7 @@ plot_heatmap <- function(bw, regions,
                          max_percentile = 0.98,
                          row_order = NULL,
                          plot_average = TRUE,
+                         average_gp = gpar(lwd = 3, col = n_groups),
                          ...) {
   require(rtracklayer)
   require(GenomicRanges)
@@ -75,7 +76,7 @@ plot_heatmap <- function(bw, regions,
     names(bw) <- str_replace(basename(bw), ".bw", "")
   }
   
- # read in coverage matrices
+  # read in coverage matrices
   message("reading coverage matrix from bigWig files")
   mat_list <-bw %>% 
     map(coverage_matrix, regions = regions, upstream = upstream, downstream = downstream, region_width = region_width) 
@@ -85,39 +86,39 @@ plot_heatmap <- function(bw, regions,
     map(is.na) %>%
     map(any) %>%
     unlist()
-
+  
   if (any(any_NA)) {
     warning("NAs detected in coverage matrices. Replacing NAs with 0")
-
+    
     replace_mat_na <- function(mat) {
       mat[is.na(mat)] <- 0
       return(mat)
     }
-
-  mat_list <- mat_list %>%
-    map(replace_mat_na)
-    }
-
+    
+    mat_list <- mat_list %>%
+      map(replace_mat_na)
+  }
+  
   
   # get row order
   if (is.null(row_order)) {
-  if(is.null(order_by_samples)) {
-    order <- mat_list %>% 
-      map(as.data.frame) %>% 
-      bind_cols %>% 
-      rowMeans() %>% 
-      order(decreasing = TRUE)
-  } else if (length(order_by_samples) == 1) {
-    order <- mat_list[[order_by_samples]] %>% 
-      rowMeans() %>% 
-      order(decreasing = TRUE)
-  } else {
-    order <- mat_list[order_by_samples] %>% 
-      map(as.data.frame) %>%
-      bind_cols() %>%
-      rowMeans() %>% 
-      order(decreasing = TRUE)
-  }
+    if(is.null(order_by_samples)) {
+      order <- mat_list %>% 
+        map(as.data.frame) %>% 
+        bind_cols %>% 
+        rowMeans() %>% 
+        order(decreasing = TRUE)
+    } else if (length(order_by_samples) == 1) {
+      order <- mat_list[[order_by_samples]] %>% 
+        rowMeans() %>% 
+        order(decreasing = TRUE)
+    } else {
+      order <- mat_list[order_by_samples] %>% 
+        map(as.data.frame) %>%
+        bind_cols() %>%
+        rowMeans() %>% 
+        order(decreasing = TRUE)
+    }
   } else {
     order  <- row_order
   }
@@ -137,20 +138,20 @@ plot_heatmap <- function(bw, regions,
   
   # get color scale for heatmaps
   if (!individual_scales) {
-  col_min <- mat_list %>%
-    map(na.exclude) %>%
-    map(min) %>%
-    unlist() %>%
-    min()
-  
-  col_max <- mat_list %>%
-    map(na.exclude) %>%
-    map(max) %>%
-    unlist() %>%
-    max()
-  
-  col_fun <- circlize::colorRamp2(seq(col_min, col_max, length.out = 9), colors)
-  
+    col_min <- mat_list %>%
+      map(na.exclude) %>%
+      map(min) %>%
+      unlist() %>%
+      min()
+    
+    col_max <- mat_list %>%
+      map(na.exclude) %>%
+      map(max) %>%
+      unlist() %>%
+      max()
+    
+    col_fun <- circlize::colorRamp2(seq(col_min, col_max, length.out = length(colors)), colors)
+    
   } 
   
   # construct heatmap_list object
@@ -160,7 +161,7 @@ plot_heatmap <- function(bw, regions,
   if (!is.null(row_split)) {
     n_groups <- seq_along(unique(row_split))
     lgd = Legend(at = sort(unique(row_split)), title = "Group", 
-                 type = "lines", legend_gp = gpar(col = n_groups))
+                 type = "lines", legend_gp = average_gp)
   } else {
     n_groups <- 1
     lgd <- NULL
@@ -169,7 +170,7 @@ plot_heatmap <- function(bw, regions,
   for (i in seq_along(mat_list)) {
     # in individual_scales and/or scale_group is set, determine separate scale for individual heatmaps or groups of heatmaps
     if (individual_scales) {
-      col_fun <- circlize::colorRamp2(seq(min(na.exclude(mat_list[[i]])), max(na.exclude(mat_list[[i]])), length.out = 9), colors)
+      col_fun <- circlize::colorRamp2(seq(min(na.exclude(mat_list[[i]])), max(na.exclude(mat_list[[i]])), length.out = length(colors)), colors)
     } 
     
     if (!is.null(scale_group)) {
@@ -179,36 +180,37 @@ plot_heatmap <- function(bw, regions,
       
       if (length(scale_group == length(mat_list))) {
         col_list <- list()
-          
-          group_i <- which(scale_group == scale_group[i])
-          
-          col_min <- mat_list[group_i] %>%
-            map(na.exclude) %>%
-            map(min) %>%
-            unlist() %>%
-            min()
-          
-          col_max <- mat_list[group_i] %>%
-            map(na.exclude) %>%
-            map(max) %>%
-            unlist() %>%
-            max()
-          
-          
-          
-          col_fun <-  circlize::colorRamp2( seq(col_min, col_max, length.out = 9), colors)
+        
+        group_i <- which(scale_group == scale_group[i])
+        
+        col_min <- mat_list[group_i] %>%
+          map(na.exclude) %>%
+          map(min) %>%
+          unlist() %>%
+          min()
+        
+        col_max <- mat_list[group_i] %>%
+          map(na.exclude) %>%
+          map(max) %>%
+          unlist() %>%
+          max()
+        
+        
+        
+        col_fun <-  circlize::colorRamp2( seq(col_min, col_max, length.out = length(colors)), colors)
         
         
       } else {
-        col_fun <- circlize::colorRamp2(seq(min(na.exclude(mat_list[[scale_group]])), max(na.exclude(mat_list[[scale_group]])), length.out = 9), colors)
+        col_fun <- circlize::colorRamp2(seq(min(na.exclude(mat_list[[scale_group]])), max(na.exclude(mat_list[[scale_group]])), length.out = length(colors)), colors)
       }
       
     } 
-
+    
+    # get range for metaplot
+    hm_range <- range(attr(col_fun, "breaks"))
+    metaplot_range <- c(hm_range[1], ceiling(hm_range[2] * 0.75))
     
     # add heatmap to list
-    
-    
     message("creating heatmap for sample ", i)
     hm_list <- hm_list + EnrichedHeatmap(mat_list[[i]],  col = col_fun,
                                          row_split = row_split,
@@ -217,18 +219,18 @@ plot_heatmap <- function(bw, regions,
                                          row_order = order,
                                          column_title = names(bw)[i], 
                                          name = names(bw)[i],
-                                         top_annotation = if(plot_average) HeatmapAnnotation(lines = anno_enriched(gp = gpar(col = n_groups))) else {NULL},
+                                         top_annotation = if (plot_average) HeatmapAnnotation(lines = anno_enriched(ylim = metaplot_range, gp = average_gp))  else {NULL},
                                          ...)
     
   }
   
   if (!return_heatmap_list) {
-  if (is.null(lgd)) {
-  draw(hm_list, merge_legends = TRUE)
-  } else {
-    draw(hm_list,annotation_legend_list = list(lgd), merge_legend = TRUE, heatmap_legend_side = "bottom", 
-         annotation_legend_side = "bottom")
-  }
+    if (is.null(lgd)) {
+      draw(hm_list, merge_legends = TRUE)
+    } else {
+      draw(hm_list,annotation_legend_list = list(lgd), merge_legend = TRUE, heatmap_legend_side = "bottom", 
+           annotation_legend_side = "bottom")
+    }
   } else {
     return(hm_list)
   }
@@ -414,3 +416,104 @@ plot_heatmap_minimal <- function(bw, regions,
   }
   
 }
+
+# function for generating metaplots/average plots ==============================
+plot_average <- function(
+    bw, 
+    regions, 
+    upstream = 1000, downstream = 1000, region_width = 1,
+    row_split = NULL,
+    colors = NULL,
+    summary_fun = mean
+) {
+  
+  require(rtracklayer)
+  require(GenomicRanges)
+  require(tidyverse)
+  
+  # check arguments
+  if (!is.character(bw)) stop("bw should be a character vector")
+  
+  if (!all(file.exists(bw))) stop("input file not found")
+  
+  if (any(!str_detect(bw, ".bw$"))) stop("bw file extension missing from input file, check input file format")
+  
+  # get sample names
+  if (is.null(names(bw))) {
+    names(bw) <- str_replace(basename(bw), ".bw", "")
+  }
+  
+  # read in coverage matrices
+  message("reading coverage matrix from bigWig file")
+  mat_list <-bw %>% 
+    map(coverage_matrix, regions = regions, upstream = upstream, downstream = downstream, region_width = region_width) 
+  
+  # check for missing values 
+  any_NA <- mat_list %>%
+    map(is.na) %>%
+    map(any) %>%
+    unlist()
+  
+  if (any(any_NA)) {
+    warning("NAs detected in coverage matrices. Replacing NAs with 0")
+    
+    replace_mat_na <- function(mat) {
+      mat[is.na(mat)] <- 0
+      return(mat)
+    }
+    
+    mat_list <- mat_list %>%
+      map(replace_mat_na)
+  }
+  
+  # convert coverage matrix to tibble
+  coverage_tb <- mat_list[[1]] %>% 
+    as.data.frame() %>% 
+    tibble()
+  
+  # add column with row groupings
+  coverage_tb$group <- row_split
+  
+  # compute average signal within groups
+  coverage_tb <- coverage_tb %>% 
+    group_by(group) %>% 
+    summarise(across(.fns = summary_fun))
+  
+  # set column names
+  colnames(coverage_tb)[2:ncol(coverage_tb)] <- 1:(ncol(coverage_tb) - 1)
+  
+  # reformat tibble for ggplot
+  coverage_tb <- coverage_tb %>% 
+    pivot_longer(2:ncol(coverage_tb), names_to = "position", values_to = "signal") %>% 
+    mutate(position = as.integer(position))
+  
+  # get position start and stop for axis labels
+  start <- min(coverage_tb$position)
+  end <- max(coverage_tb$position)
+  
+  # generate plot
+  p <- coverage_tb %>% 
+    ggplot(aes(x = position, y = signal, color = group)) + geom_line(lwd = 2) +
+    coord_cartesian(clip = "off") +
+    viridis::scale_color_viridis(discrete=TRUE) +
+    theme(text = element_text(size = 16),
+          panel.border = element_rect(color = "black", fill=NA, size=1),
+          axis.ticks = element_line(colour = "black", size = 1),
+          axis.ticks.length=unit(.2, "cm"),
+          panel.background = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          axis.title.x = element_blank()
+    ) + 
+    scale_x_continuous(
+      
+      breaks = c(start,end/2,end), 
+      labels = c(paste0("-", upstream / 1000, "KB"),"center", paste0("+", upstream / 1000, "KB")),
+      expand = c(0,0,0,0)
+      ) 
+  
+  # return plot
+  return(p)
+  
+}
+
